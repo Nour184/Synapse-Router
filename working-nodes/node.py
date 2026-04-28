29e3552f10fa5991a3af16494256b100fbb6df14
Nour Mamdouh <nourelbakry06@gmail.com>
2026-04-29 00:22:08 +0300

    set up docker containers and python dependencies

--- working-nodes/node.py
+++ working-nodes/node.py
@@ -1,1 +1,8 @@
-{\rtf1}
+from fastapi import FastAPI
+
+# This is the "app" variable that uvicorn is looking for!
+app = FastAPI() 
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello from the Worker Node!"}
